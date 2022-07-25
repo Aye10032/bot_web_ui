e0e0ee8d8767deb05df621625daf3043239f10b7
Aye10032 <aye10032@gmail.com>
2022-07-25 17:29:07 +0800

    init project

---
+++ src/main/java/com/aye10032/bot_web_ui/BotWebUiApplication.java
@@ -0,0 +1,13 @@
+package com.aye10032.bot_web_ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotWebUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotWebUiApplication.class, args);
+    }
+
+}
