d4745adad1f1911fffbb86f0003b9bcb8ee7d71a
Aye10032 <aye10032@gmail.com>
2022-07-26 18:23:09 +0800

    添加静态资源

---
+++ src/main/java/com/aye10032/bot_web_ui/controller/IndexController.java
@@ -0,0 +1,23 @@
+package com.aye10032.bot_web_ui.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @program: bot_web_ui
+ * @className: IndexController
+ * @Description:
+ * @version: v1.0
+ * @author: Aye10032
+ * @date: 2022/7/25 下午 7:18
+ */
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+}
